d84b19f5e6430aa28b0201707447c7ee39529c94
krishmamaharjan <krishmamaharjan73@gmail.com>
2025-04-16 21:37:36 +0545

    react-component

---
+++ src/App.js
@@ -0,0 +1,24 @@
+// import logo from './logo.svg';
+// import React from 'react';
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+
+ 
+  return (
+    <div className="App">
+     <Header />
+    
+    </div>
+  );
+}
+
+
+export default App;
+
+// class App extends React.Component {
+//   render(){
+//     return <h1>Hello from a class</h1>
+//   }
+// }
