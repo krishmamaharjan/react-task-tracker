65d36d437c505bd4fa294b34dc8134e521240ef6
krishmamaharjan <krishmamaharjan73@gmail.com>
2025-04-17 21:49:19 +0545

    button-event component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,32 @@
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
+import '../index.css';
+import Button from './Button'
+const Header = (props) => {
+const onClick = () => {
+  console.log('Click')
+}
 
-const Header = ({title}) => {
   return (
     <header>
-        <h1>{title}</h1>
+        <h1 >{props.title}</h1>
+        <Button color='green' text='Add'
+        onClick = {onClick}
+        />
+        
     </header>
   )
 }
 
-Header.defaultProps = {
-    title: 'Task tracker',
-}
+// Header.defaultProps = {
+//     title: 'Task tracker',
+// }
+// Header.propTypes = {
+//   title: PropTypes.string.isRequired,
+// }
+
+//CSS in Js
+// const headingStyle = {
+//   color: 'red', 
+//   background: 'black' 
+// }
 export default Header
