d755a935c5b83cc9b38937ecc0fe9f59bf4848d3
krishmamaharjan <krishmamaharjan73@gmail.com>
2025-04-18 17:37:36 +0545

    Task-component added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
 // import logo from './logo.svg';
 // import React from 'react';
-import './App.css';
+// import './App.css';
+
+import {useState} from 'react';
+
+import './index.css';
 import Header from './components/Header';
+import Task from './components/Tasks'
 
 function App() {
 
- 
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Doctors Appointment',
+      day: 'Feb 5th at 2:30pm',
+      reminder: true,
+    },
+    {
+      id:2,
+      text:'Meeting at School',
+      day: 'Feb 6th at 1:30pm',
+      reminder: true,
+    },
+    {
+      id:3,
+      text:'Food Shopping',
+      day: 'Feb 6th at 4:30pm',
+      reminder: false,
+    }
+  ]
+  )
+  
+ // Delete Task
+
+ const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !== id))
+ }
   return (
     <div className="App">
-     <Header />
+     <Header title='Task Tracker'/>
+    {tasks.length > 0 ? 
+    <Task  tasks={tasks} onDelete={deleteTask}/>
+      : 'No tasks To Show'
+  }
     
     </div>
   );
